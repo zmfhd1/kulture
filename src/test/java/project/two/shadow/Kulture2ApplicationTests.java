97ea0532441aba8f99dc8872bdca057cc8126156
zmfhd1 <lfwm21@gmail.com>
2021-11-02 09:58:46 +0900

    first commit

---
+++ src/test/java/project/two/shadow/Kulture2ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.two.shadow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kulture2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
